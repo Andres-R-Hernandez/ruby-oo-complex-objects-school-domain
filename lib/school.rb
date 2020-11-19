adea99cfb627f36b366fca40a090475061db9b62
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-11-18 20:20:00 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(class_grade)
+    @roster[class_grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
